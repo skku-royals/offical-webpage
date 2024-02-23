a63cbc505aaa2d5e1d6768b6d27ca4cad1246c4e
jspark2000 <idpjs2000@gmail.com>
2024-02-23 15:07:54 +0000

    fix: modify security groups

--- aws/royals/security-group.tf
+++ aws/royals/security-group.tf
@@ -20,11 +20,12 @@ resource "aws_security_group" "ec2" {
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-    description = "Allow all outbound traffic"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+    description      = "Allow all outbound traffic"
   }
 
   tags = {
