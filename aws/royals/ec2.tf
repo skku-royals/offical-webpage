311dd3b16f4cd7e746df7e8819ea851764938c21
jspark2000 <idpjs2000@gmail.com>
2024-02-23 15:45:45 +0000

    fix: add root block device settings on ec2

--- aws/royals/ec2.tf
+++ aws/royals/ec2.tf
@@ -4,6 +4,12 @@ resource "aws_instance" "database" {
   subnet_id              = aws_subnet.private_1.id
   vpc_security_group_ids = [aws_security_group.ec2.id]
 
+  root_block_device {
+    volume_size           = 50
+    volume_type           = "gp3"
+    delete_on_termination = true
+  }
+
   user_data = templatefile("${path.module}/postgres-docker.sh.tpl", { postgres_password = var.postgres_password })
 
   tags = {
@@ -16,6 +22,12 @@ resource "aws_instance" "cache" {
   instance_type          = "t4g.micro"
   subnet_id              = aws_subnet.private_2.id
   vpc_security_group_ids = [aws_security_group.ec2.id]
+
+  root_block_device {
+    volume_size           = 15
+    volume_type           = "gp3"
+    delete_on_termination = true
+  }
 
   user_data = templatefile("${path.module}/redis-docker.sh.tpl", {})
 
