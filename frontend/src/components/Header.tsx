fbbbbafb782c046e8f26fce312e70044818163a2
jspark2000 <idpjs2000@gmail.com>
2024-02-27 14:42:18 +0000

    fix: header components

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -31,7 +31,7 @@ export default function Header() {
   }, [])
 
   return (
-    <header className="fixed left-0 top-0 z-10 w-full border-b border-zinc-200 bg-transparent backdrop-blur-md dark:border-zinc-600">
+    <header className="fixed left-0 top-0 z-10 w-full border-b border-zinc-200 bg-transparent backdrop-blur-sm dark:border-zinc-600">
       <nav
         className="mx-auto flex max-w-7xl items-center justify-between px-6 py-3.5 lg:px-8"
         aria-label="Global"
@@ -85,8 +85,8 @@ export default function Header() {
         open={mobileMenuOpen}
         onClose={setMobileMenuOpen}
       >
-        <div className="fixed inset-0 z-10" />
-        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto px-6 py-6 backdrop-blur-md sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+        <div className="fixed inset-0 z-20" />
+        <Dialog.Panel className="fixed inset-y-0 right-0 z-20 w-full overflow-y-auto bg-white px-6 pt-[20px] sm:max-w-sm sm:ring-1 sm:ring-gray-900/10 dark:bg-black">
           <div className="flex items-center justify-between">
             <Link
               href="/"
@@ -111,7 +111,7 @@ export default function Header() {
               <XMarkIcon className="h-5 w-5" aria-hidden="true" />
             </button>
           </div>
-          <div className="mt-6 flow-root">
+          <div className="mt-8 flow-root">
             <div className="-my-6 divide-y-2 divide-gray-500/10">
               <div className="space-y-2 py-6">
                 {navigation.map((item) => (
