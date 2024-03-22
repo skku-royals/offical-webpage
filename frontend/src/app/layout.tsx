11cb0ecd5a32b73a5c41a3f2db784f9be926f1cf
jspark2000 <idpjs2000@gmail.com>
2024-03-22 22:29:08 +0000

    feat: configure apple web app

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import { ThemeProvider } from '@/components/ThemeProvider'
 import Toaster from '@/components/ui/sooner'
 import { cn } from '@/lib/utils'
-import type { Metadata } from 'next'
+import type { Metadata, Viewport } from 'next'
 import { Noto_Sans_KR } from 'next/font/google'
 import './globals.css'
 
@@ -11,10 +11,71 @@ const noto = Noto_Sans_KR({
   variable: '--font-noto'
 })
 
+export const viewport: Viewport = {
+  themeColor: '#141517'
+}
+
 export const metadata: Metadata = {
   title: '성균관대학교 미식축구부 로얄스',
   description: '성균관대학교 미식축구부 로얄스 홈페이지입니다',
-  metadataBase: new URL('https://skku-royals.com')
+  metadataBase: new URL('https://skku-royals.com'),
+  appleWebApp: {
+    title: 'ROYALS',
+    statusBarStyle: 'black-translucent',
+    startupImage: [
+      'splashscreens/iphonex_splash.png',
+      {
+        url: 'splashscreens/iphone5_splash.png',
+        media:
+          '(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)'
+      },
+      {
+        url: 'splashscreens/iphone6_splash.png',
+        media:
+          '(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)'
+      },
+      {
+        url: 'splashscreens/iphoneplus_splash.png',
+        media:
+          '(device-width: 621px) and (device-height: 1104px) and (-webkit-device-pixel-ratio: 3)'
+      },
+      {
+        url: 'splashscreens/iphonex_splash.png',
+        media:
+          '(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)'
+      },
+      {
+        url: 'splashscreens/iphonexr_splash.png',
+        media:
+          '(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)'
+      },
+      {
+        url: 'splashscreens/iphonexsmax_splash.png',
+        media:
+          '(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)'
+      },
+      {
+        url: 'splashscreens/ipad_splash.png',
+        media:
+          '(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)'
+      },
+      {
+        url: 'splashscreens/ipadpro1_splash.png',
+        media:
+          '(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)'
+      },
+      {
+        url: 'splashscreens/ipadpro3_splash.png',
+        media:
+          '(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)'
+      },
+      {
+        url: 'splashscreens/ipadpro2_splash.png',
+        media:
+          '(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)'
+      }
+    ]
+  }
 }
 
 export default function RootLayout({
