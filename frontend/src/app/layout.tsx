89c45604f7ca94cc8f73742e3c806f33a37e7fc7
jspark2000 <idpjs2000@gmail.com>
2024-03-22 22:37:33 +0000

    fix: modify typo

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -23,54 +23,54 @@ export const metadata: Metadata = {
     title: 'ROYALS',
     statusBarStyle: 'black-translucent',
     startupImage: [
-      'splashscreens/iphonex_splash.png',
+      '/splashscreens/iphonex_splash.png',
       {
-        url: 'splashscreens/iphone5_splash.png',
+        url: '/splashscreens/iphone5_splash.png',
         media:
           '(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)'
       },
       {
-        url: 'splashscreens/iphone6_splash.png',
+        url: '/splashscreens/iphone6_splash.png',
         media:
           '(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)'
       },
       {
-        url: 'splashscreens/iphoneplus_splash.png',
+        url: '/splashscreens/iphoneplus_splash.png',
         media:
           '(device-width: 621px) and (device-height: 1104px) and (-webkit-device-pixel-ratio: 3)'
       },
       {
-        url: 'splashscreens/iphonex_splash.png',
+        url: '/splashscreens/iphonex_splash.png',
         media:
           '(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)'
       },
       {
-        url: 'splashscreens/iphonexr_splash.png',
+        url: '/splashscreens/iphonexr_splash.png',
         media:
           '(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)'
       },
       {
-        url: 'splashscreens/iphonexsmax_splash.png',
+        url: '/splashscreens/iphonexsmax_splash.png',
         media:
           '(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)'
       },
       {
-        url: 'splashscreens/ipad_splash.png',
+        url: '/splashscreens/ipad_splash.png',
         media:
           '(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)'
       },
       {
-        url: 'splashscreens/ipadpro1_splash.png',
+        url: '/splashscreens/ipadpro1_splash.png',
         media:
           '(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)'
       },
       {
-        url: 'splashscreens/ipadpro3_splash.png',
+        url: '/splashscreens/ipadpro3_splash.png',
         media:
           '(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)'
       },
       {
-        url: 'splashscreens/ipadpro2_splash.png',
+        url: '/splashscreens/ipadpro2_splash.png',
         media:
           '(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)'
       }
